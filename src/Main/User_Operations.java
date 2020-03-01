e926e886e7082b9aa653610d40a6d6426b0f6b66
Biggie Cheese <50361292+GitHubMaster69@users.noreply.github.com>
2020-03-01 03:18:56 +0100

    Add-,Remove-,Update- favorites

--- src/Main/User_Operations.java
+++ src/Main/User_Operations.java
@@ -22,4 +22,48 @@ public class User_Operations extends Operations {
             e.printStackTrace();
         }
     }
+    public void addFavorite() {
+        try  {
+            String addFavQuery = "INSERT INTO favorites VALUES (?,?,?);";
+            PreparedStatement pst = c.prepareStatement(addFavQuery);
+
+            pst.setString(1,"1"); // pro_id
+            pst.setString(2, "dummy"); //mov_sta_id
+            pst.setString(3, "100"); //mov_id
+
+            pst.execute();
+            System.out.println("Favorite added succesfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void removeFavorite() {
+        try  {
+            String remFavQuery = "DELETE FROM favorites WHERE mov_id = ?";
+            PreparedStatement pst = c.prepareStatement(remFavQuery);
+
+            pst.setString(1,"1");
+
+            pst.executeUpdate();
+            System.out.println("Favorite removed succesfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void updateFavorite() {
+        try  {
+            String upFavQuery = "UPDATE favorites SET mov_sta_id = ?;";
+            PreparedStatement pst = c.prepareStatement(upFavQuery);
+
+            pst.setString(1,"1"); // mov_sta_id
+
+            pst.executeUpdate();
+            System.out.println("updated succesfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
